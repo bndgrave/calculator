ee856533ec9056389c2857ced4029a5b4c0e4ac3
Дмитрий Ошмарин <bndgrave1@gmail.com>
2022-05-30 00:11:22 +0500

    Commit 2: Correct realization

--- sp_12_task_2_calc.py
+++ sp_12_task_2_calc.py
@@ -13,27 +13,24 @@ class Stack:
         return value
 
 
-class Calculator:
-    def __init__(self,expression:str):
-        self.expression = expression
-        self.operators = Stack()
-        self.operands = Stack()
-    
-    def decode_expression(self):
-        items = self.expression.split(' ')
-        for item in items:
-            if item.isdigit():
-                self.operands.push(item)
-            else:
-                self.operators.push(item)
-
-    def calc_result(self):
-        while not self.operators.is_empty():
-            operator = self.operators.pop()
-            operands = [self.operands.pop() for index in range(2)]
-            self.operands.push(
+def calc_result(expression:str):
+    elements = expression.split(' ')
+    operands = Stack()
+    for element in elements:
+        if element.isdigit():
+            operands.push(element)
+        else:
+            operator = '//' if element == '/' else element
+            pair_to_calc = [operands.pop() for index in range(2)]
+            operands.push(
                 str(
-                    eval(operands[1] + operator + operands[0])
+                    eval(pair_to_calc[1] + operator + pair_to_calc[0])
                 )
             )
-        return int(self.operands.pop())
+    return operands.pop()
+
+
+if __name__ == '__main__':
+    expression = input()
+    result = calc_result(expression)
+    print(result)
